91439d1d164e59fa83479f6e206e927865898b82
Jarmo Kuusisto <jarmokuu@gmail.com>
2018-11-23 12:27:51 +0200

    project frontend

---
+++ README.md
@@ -0,0 +1,2 @@
+# Full Stack OSA 3
+* [Tehtävä 3.10](https://fs-puhelinmuistio.herokuapp.com/)
---
+++ build/precache-manifest.c6887d71bcf8f311c87ccbfaa33e87b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2c117bf844b97375229",
+    "url": "/static/js/main.c2c117bf.chunk.js"
+  },
+  {
+    "revision": "3b1846dbd9e8365c15a3",
+    "url": "/static/js/1.3b1846db.chunk.js"
+  },
+  {
+    "revision": "c2c117bf844b97375229",
+    "url": "/static/css/main.e0d3a06e.chunk.css"
+  },
+  {
+    "revision": "bc3434d1dcb249e328fb34ccede6da33",
+    "url": "/index.html"
+  }
+];
